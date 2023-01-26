3cb72da59f5fe48734b83e58590b5f5b45e156dc
Zeyneb Mehlika Sarıyıldız <sariyildiz.mehlika07@gmail.com>
2023-01-26 10:40:13 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klavteden_girilen_2_sayıyı_çıkaran_uygulama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1, sayi2,fark;
+            Console.WriteLine("Lütfen Bir Sayı Giriniz");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Lütfen  Bir Sayı Daha Giriniz");
+            sayi2 = Convert.ToInt32((Console.ReadLine()));
+            fark = sayi1 - sayi2;
+            {
+                Console.WriteLine("Girdiğiniz Sayıların Farkı = {0}", fark);
+            }
+            Console.ReadKey();
+        }
+    }
+}
